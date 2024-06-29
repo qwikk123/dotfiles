03733b97ff2b7388c220ad351235013189dd04cd
qwikk <xd :)>
2024-06-29 13:02:28 +0200

    :)

---
+++ nix/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    nixosConfigurations = {
+
+      qwikk = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+	  ./configuration.nix
+	  ./hardware-configuration.nix
+        ];	
+      };
+    };
+
+
+  };
+}
